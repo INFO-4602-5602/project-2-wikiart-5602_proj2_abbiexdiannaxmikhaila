5866ebb26378f943390569d11fd656365a146143
Mikhaila Friske <frisk028@umn.edu>
2019-12-02 22:31:19 -0700

    add some scrolling and update styles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
+var orgscroll = $(document).scrollTop(), switchcheck=false;
+
+function autoScroll(){
+  // if(scrollable){
+    if(switchcheck) return;
+    switchcheck = true;
+    var viz = ['one', 'two', 'two-b', 'three', 'four']
+    var scroll = $(this).scrollTop();
+    // scrollable=false;
+    
+    if (scroll > orgscroll) {
+
+      for (var i in viz) {
+        num = viz[i];
+        id = '#viz-'+num;
+        if (scroll < $(id).offset().top + 1) {
+          $('html, body').animate({ scrollTop: $(id).offset().top }, 'slow');
+          break;
+        }
+      }
+    }
+    orgscroll = scroll;
+  // }
+  setTimeout(function(){ switchcheck = false; }, 1000)
+}
+
+$(window).on('scroll', autoScroll);
+
+
 $(document).foundation()
